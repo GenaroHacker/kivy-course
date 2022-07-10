a97854b8081dbc9bb9d5f6f6d37c32e75e75951c
GenaroHacker <genarominetto@hotmail.com>
2022-07-11 00:10:19 +0200

    GridLayout, labels, entrys y numero de columnas

--- first_video.py
+++ first_video.py
@@ -1,13 +1,29 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 
 
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='Name:'))
+        self.name = TextInput(multiline=False)
+        self.add_widget(self.name)
+        
+        self.add_widget(Label(text='Age:'))
+        self.age = TextInput(multiline=False)
+        self.add_widget(self.age)
 
+        self.add_widget(Label(text='Job:'))
+        self.job = TextInput(multiline=False)
+        self.add_widget(self.job)
 
 class MyApp(App):
     def build(self):
-        return Label(text='Tech with tim')
+        return MyGrid()
 
 if __name__ == "__main__":
     MyApp().run()
