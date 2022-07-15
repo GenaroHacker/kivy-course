aceac43d281f06d82dd709e8f50b750e2847038d
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2022-07-15 14:19:20 +0200

    cuadrado sigue al puntero

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.graphics import Rectangle
+from kivy.graphics import Color
+from kivy.graphics import Line
 
 class touch(Widget):
-    btn = ObjectProperty(None)
+    def __init__(self, **kwargs):
+        super(touch, self).__init__(**kwargs)
+
+        with self.canvas:
+            Color(1, 0, 0)
+            Line(points=(100, 100, 200, 200, 300, 300))
+            Color(1, 0, 0, 1)
+            self.rect = Rectangle(pos=(0, 0), size=(50, 50))
+
 
     def on_touch_down(self, touch):
+        self.rect.pos = touch.pos
         print("Mouse Down", touch)
-        self.btn.opacity = 0.5
         
     def on_touch_move(self, touch):
+        self.rect.pos = touch.pos
         print("Mouse Move", touch)
-
-    def on_touch_up(self, touch):
-        print("Mouse Up", touch)
-        self.btn.opacity = 1
 
 class MyApp(App):
     def build(self):
